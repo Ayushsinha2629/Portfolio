28669fb5c819c424d1ce2bea5a8aae79179ba2d5
Ayushsinha2629 <ayush61187@gmail.com>
2024-10-20 18:34:26 +0530

    Add: About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
+import About from './components/About'
+import Landing from './components/Landing'
 import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='overflow-hidden text-neutral-300 antialiased selection:bg-blue-700 selection:text-blue-900'>
-      <div className='fixed top-0 z-10 h-full w-full'></div>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className='fixed top-0 -z-10 h-full w-full'>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
       
       <div className='container mx-auto'>
         <Navbar/> 
+        <Landing/>
+        <About/>
       </div>
     </div>
   )
