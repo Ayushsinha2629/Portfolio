7b173ec975ff0ad96a1c8284c60d4215ddf85d6c
Ayushsinha2629 <ayush61187@gmail.com>
2024-10-20 23:58:02 +0530

    Refactor: Text size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,10 @@ import { ABOUT_TEXT } from '../constants'
 const About = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h2 className='my-20 text-center text-4xl'>
+        <h1 className='my-20 text-center text-4xl'>
           About
-        </h2>
+          <span className='text-slate-400'> Me</span>
+        </h1>
         <div className='flex flex-wrap'>
           <div className='w-full lg:w-1/2 lg:p-8'>
             <div className='flex items-center justify-center'>
