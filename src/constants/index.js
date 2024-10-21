7f47530d14b36d0afb3f7b879d2ca8f7fc15669a
Ayushsinha2629 <ayush61187@gmail.com>
2024-10-21 17:37:20 +0530

    Add: Content for the UI

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,76 +1,62 @@
-import project1 from "../assets/projects/project-1.jpg";
+import medium from "../assets/projects/medium.png";
 import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import passop from "../assets/projects/passop.png";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const LANDING_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 6+ months of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 6+ months of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "June 2024 - Present",
+    role: "SDE Intern",
+    company: "i4Sight Technologies.",
+    description: `Contributed to the development of a scalable web application using React, Django, and Axios, significantly
+      reducing the reliance on Django Admin.
+      This streamlined administrative workflows and improved overall system efficiency.
+
+      Designed and implemented comprehensive report forms with React and Django, resulting in a 85% reduction in
+      data entry time.`,
+    technologies: ["Javascript", "React.js", "Python", "Django", "Rest Framework"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+    year: "June 2023 - August 2023",
+    role: "Team Lead - Social Internship",
+    company: "Harmonious Children Rehabilitation Center",
+    description: `Conducted regular progress reviews with individual team members to identify areas for improvement and provide guidance on how to effectively care for children. Built strong partnerships with parents, educators, and community organizations, increasing engagement and support for children's rehabilitation by 75%`,
+    technologies: ["Leadership", "Communication", "Teamwork"],
+  }
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    image: project1,
+    title: "Medium - Write your Blogs!",
+    image: medium,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+      "A responsive blogging platform with user authentication and post management. Features include efficient database operations with Prisma, PostgreSQL, and schema validation using Zod. Deployed on Vercel for optimized performance with Prisma Accelerate.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    url: "https://medium-clone-68bmo4x1f-ayushsinha2629s-projects.vercel.app/signup"
   },
   {
-    title: "Task Management App",
+    title: "SimplePay - Payment Gateway",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+      "A finance app for managing accounts, transferring funds, and searching recipients. It integrates robust Next-Auth user authentication, PostgreSQL for transaction integrity, and a responsive UI built with Next.js and Tailwind CSS.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    url: ""
   },
   {
-    title: "Portfolio Website",
-    image: project3,
+    title: "PassOp - Password Manager",
+    image: passop,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+      "A secure password management app with full CRUD functionality. It features encrypted storage using MongoDB, an intuitive React.js interface styled with Tailwind CSS, and ensures secure handling of sensitive data.",
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    url: "https://pass-manager-1.onrender.com/"
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Paschim Vihar, New Delhi, India",
+  phoneNo: "+91 8448531810 ",
+  email: "ayushsinhaa7@gmail.com",
 };
